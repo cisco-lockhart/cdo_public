6cb9751a9faed9163f8d81c547e98a7310e2df89
Siddhu Warrier <siwarrie@cisco.com>
2018-03-27 15:22:11 -0700

    hackathon: - Write to notes API
    Change-Id: Id4f9be36f0a004037acde1ba10ad4bc30fa3c52d

--- example_apps/python/pkg/download/asa_configs.py
+++ example_apps/python/pkg/download/asa_configs.py
@@ -9,7 +9,6 @@ from ..utils import *
 from .. import envutils
 from sty import fg, ef, rs
 
-GET_DEVICES_URL = "{0}/aegis/rest/v1/services/targets/devices"
 
 NUM_DEVICES_TO_RETRIEVE_PER_QUERY = 50
 
@@ -20,6 +19,8 @@ def download_asa_configs(api_token, env, output_dir):
     print(download_msg, end='\r')
 
     filenames = []
+    device_uids = []
+    device_names = []
     animation = '|/-\\'
     for i in range(0, num_devices, NUM_DEVICES_TO_RETRIEVE_PER_QUERY):
         params = {
@@ -30,8 +31,8 @@ def download_asa_configs(api_token, env, output_dir):
             'offset': i
         }
 
-        response = requests.get(url=_get_devices_url(env),
-                                headers=_get_headers(api_token),
+        response = requests.get(url=envutils.get_devices_url(env),
+                                headers=envutils.get_headers(api_token),
                                 params=params)
 
         if not os.path.exists(output_dir):
@@ -45,11 +46,12 @@ def download_asa_configs(api_token, env, output_dir):
             config_file.close()
             filenames.append(config_file.name)
 
-
+            device_uids.append(device_json['uid'])
+            device_names.append(device_json['name'])
             print(display_in_progress_animation(download_msg, i), end='\r')
 
     print(as_done_msg(download_msg))
-    return filenames
+    return (device_uids, device_names, filenames)
 
 
 def _get_device_count(env, api_token):
@@ -57,19 +59,8 @@ def _get_device_count(env, api_token):
         'q': '(deviceType:ASA)',
         'agg': 'count'
     }
-    response = requests.get(url=_get_devices_url(env),
-                            headers=_get_headers(api_token),
+    response = requests.get(url=envutils.get_devices_url(env),
+                            headers=envutils.get_headers(api_token),
                             params=params)
 
     return json.loads(response.text)['aggregationQueryResult']
-
-
-def _get_headers(api_token):
-    return {
-        "Authorization": "Bearer " + api_token,
-        "Content-Type": "application/json"
-    }
-
-
-def _get_devices_url(env):
-    return GET_DEVICES_URL.format(envutils.get_base_url(env))
--- example_apps/python/pkg/envutils/envutils.py
+++ example_apps/python/pkg/envutils/envutils.py
@@ -4,6 +4,10 @@ STAGING_BASE_URL = "https://staging.dev.lockhart.io"
 EU_BASE_URL = "https://www.defenseorchestrator.eu"
 LOCALHOST_BASE_URL = "http://localhost:9000"
 
+DEVICES_URL = "{0}/aegis/rest/v1/services/targets/devices"
+
+ANALYSIS_RESULTS_URL = "{0}/aegis/rest/v1/services/analysis/results"
+NOTES_URL = "{0}/aegis/rest/v1/services/common/notes"
 
 def get_base_url(env):
     if env == 'us':
@@ -14,3 +18,22 @@ def get_base_url(env):
         return EU_BASE_URL
     else:
         return LOCALHOST_BASE_URL
+
+
+def get_devices_url(env):
+    return DEVICES_URL.format(get_base_url(env))
+
+
+def get_analysis_results_url(env):
+    return ANALYSIS_RESULTS_URL.format(get_base_url(env))
+
+def get_notes_url(env):
+    return NOTES_URL.format(get_base_url(env))
+
+
+def get_headers(api_token):
+    return {
+        "Authorization": "Bearer " + api_token,
+        "Content-Type": "application/json"
+    }
+
