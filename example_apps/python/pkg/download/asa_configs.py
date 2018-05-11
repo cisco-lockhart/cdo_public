1e1e18f1d785cf9cdff3f0bcc58f783a1e9f181d
Siddhu Warrier <siwarrie@cisco.com>
2018-05-11 09:39:09 +0100

    devnet-2641-step-1: - Create basic skeleton code for downloading configs
    Change-Id: Id11980c965e509d84f16c336c0d04ab2ec293a4f

--- example_apps/python/pkg/download/asa_configs.py
+++ example_apps/python/pkg/download/asa_configs.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python
 
+import requests
+import json
+import os
+import sys
 from ..utils import *
 
+from .. import envutils
+from sty import fg, ef, rs
+
+
+NUM_DEVICES_TO_RETRIEVE_PER_QUERY = 50
 
 def download_asa_configs(api_token, env, output_dir):
-    download_msg = as_in_progress_msg('Downloading configurations for ' + str(num_devices) + ' ASAs from CDO environment: ' + env + '...')
+
+    download_msg = as_in_progress_msg('Downloading configurations for ASAs from CDO environment: ' + env + '...')
     print(download_msg, end='\r')
+
     print(as_done_msg(download_msg))
 
+
+def _save_device_config(device_name, device_config, output_dir):
+    output_file = open(os.path.join(output_dir, device_name), 'w')
+    output_file.write(device_config)
