65c8f4a1a86e65ac9adef927ec58829129101795
Siddhu Warrier <siwarrie@cisco.com>
2018-05-11 17:39:36 +0100

    devnet-2641-step-1:
    Change-Id: I76c7e67d16e904030253122343d498e3cb052b8c

--- example_apps/python/pkg/download/asa_configs.py
+++ example_apps/python/pkg/download/asa_configs.py
@@ -12,12 +12,13 @@ from sty import fg, ef, rs
 
 NUM_DEVICES_TO_RETRIEVE_PER_QUERY = 50
 
+
 def download_asa_configs(api_token, output_dir):
-
     download_msg = as_in_progress_msg('Downloading configurations for ASAs from CDO...')
-    print(download_msg, end='\r')
-
-    print(as_done_msg(download_msg))
+    print(download_msg, end='')
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    print(as_done_msg(''))
 
 
 def _save_device_config(device_name, device_config, output_dir):
