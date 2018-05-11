b2c7727c5ea1cf48a4ce96eb1ac6bd5e887cfd08
Siddhu Warrier <siwarrie@cisco.com>
2018-05-11 09:49:54 +0100

    devnet-2641-step-2: - Retrieve all devices of type ASA
    Change-Id: I596bc3fdff2cb704138f46d68aa1a10cb94160eb

--- example_apps/python/pkg/download/asa_configs.py
+++ example_apps/python/pkg/download/asa_configs.py
@@ -17,8 +17,18 @@ def download_asa_configs(api_token, env, output_dir):
     download_msg = as_in_progress_msg('Downloading configurations for ASAs from CDO environment: ' + env + '...')
     print(download_msg, end='\r')
 
-    print(as_done_msg(download_msg))
+    params = {
+        'q': '(deviceType:ASA)',
+    }
+    headers = {
+        "Authorization": "Bearer " + api_token,
+        "Content-Type": "application/json"
+    }
+    response = requests.get(url='https://www.defenseorchestrator.com/aegis/rest/v1/services/targets/devices',
+                            headers=headers,
+                            params=params)
 
+    print(as_done_msg(download_msg))
 
 def _save_device_config(device_name, device_config, output_dir):
     output_file = open(os.path.join(output_dir, device_name), 'w')
