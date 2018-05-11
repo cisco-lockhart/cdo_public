e4b75d8c86ada6d90b2a74f722fb8673aeac1ace
Siddhu Warrier <siwarrie@cisco.com>
2018-05-11 14:48:03 +0100

    add-onboard-model-cmd: - Add ability to bulk onboard model devices
    Change-Id: I2afd9ac765a92b6e64eb44a246d6e45f74fa8fc5

--- example_apps/python/pkg/download/asa_configs.py
+++ example_apps/python/pkg/download/asa_configs.py
@@ -1,35 +1,66 @@
 #!/usr/bin/env python
 
-import json
-
-import os
 import requests
-
+import json
+import os
+import sys
 from ..utils import *
+
+from .. import envutils
+from sty import fg, ef, rs
+
 
 NUM_DEVICES_TO_RETRIEVE_PER_QUERY = 50
 
 def download_asa_configs(api_token, env, output_dir):
+    num_devices = _get_device_count(env, api_token)
 
-    download_msg = as_in_progress_msg('Downloading configurations for ASAs from CDO environment: ' + env + '...')
+    download_msg = as_in_progress_msg('Downloading configurations for ' + str(num_devices) + ' ASAs from CDO environment: ' + env + '...')
     print(download_msg, end='\r')
 
-    params = {
-        'q': '(deviceType:ASA)',
-    }
-    headers = {
-        "Authorization": "Bearer " + api_token,
-        "Content-Type": "application/json"
-    }
-    response = requests.get(url='https://www.defenseorchestrator.com/aegis/rest/v1/services/targets/devices',
-                            headers=headers,
-                            params=params)
-    devices_json = json.loads(response)
-    for device_json in devices_json:
-        _save_device_config(device_name=device_json['name'], device_config=device_json['deviceConfig'], output_dir=output_dir)
+    filenames = []
+    device_uids = []
+    device_names = []
+    animation = '|/-\\'
+    for i in range(0, num_devices, NUM_DEVICES_TO_RETRIEVE_PER_QUERY):
+        params = {
+            'q': '(deviceType:ASA)',
+            'resolve': '[targets/devices.{name,deviceConfig}]',
+            'sort': 'name:desc',
+            'limit': NUM_DEVICES_TO_RETRIEVE_PER_QUERY,
+            'offset': i
+        }
+
+        response = requests.get(url=envutils.get_devices_url(env),
+                                headers=envutils.get_headers(api_token),
+                                params=params)
+
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        devices_json = json.loads(response.text)
+        for device_json in devices_json:
+            config_file = open(os.path.join(output_dir, device_json['name'] + '.config.txt'), "w")
+            if device_json['deviceConfig'] is not None:
+                config_file.write(device_json['deviceConfig'])
+            config_file.close()
+            filenames.append(config_file.name)
+
+            device_uids.append(device_json['uid'])
+            device_names.append(device_json['name'])
+            print(display_in_progress_animation(download_msg, i), end='\r')
 
     print(as_done_msg(download_msg))
+    return (device_uids, device_names, filenames)
 
-def _save_device_config(device_name, device_config, output_dir):
-    output_file = open(os.path.join(output_dir, device_name), 'w')
-    output_file.write(device_config)
+
+def _get_device_count(env, api_token):
+    params = {
+        'q': '(deviceType:ASA)',
+        'agg': 'count'
+    }
+    response = requests.get(url=envutils.get_devices_url(env),
+                            headers=envutils.get_headers(api_token),
+                            params=params)
+
+    return json.loads(response.text)['aggregationQueryResult']
---
+++ example_apps/python/pkg/onboard/__init__.py
@@ -0,0 +1,1 @@
+from .onboarder import *
