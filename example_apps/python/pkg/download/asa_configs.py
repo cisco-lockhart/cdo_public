bec8241c379e137698d4f75e40f1d08e8154308e
Siddhu Warrier <siwarrie@cisco.com>
2018-06-12 17:13:13 -0400

    devnet-2641-step-4: - Done
    Change-Id: Id2b4719b0cd91edce7f2062f88f2a2bdaa736c46

--- example_apps/python/pkg/download/asa_configs.py
+++ example_apps/python/pkg/download/asa_configs.py
@@ -4,7 +4,8 @@ from . import cdoapi
 
 API_TOKEN=os.getenv('API_TOKEN')
 def download_asa_configs(api_token, output_dir):
-    cdoapi.get_num_devices(API_TOKEN)
-    devices = cdoapi.get_devices(API_TOKEN)
-    for device in devices:
-        cdoapi.save_device_config(device, output_dir)
+    num_devices = cdoapi.get_num_devices(API_TOKEN)
+    for offset in range(0, num_devices):
+        devices = cdoapi.get_devices(API_TOKEN, offset)
+        for device in devices:
+            cdoapi.save_device_config(device, output_dir)
