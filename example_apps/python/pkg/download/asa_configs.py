842677504cb25bfb7c5d551ab25cfbacc2ff40e9
Siddhu Warrier <siwarrie@cisco.com>
2018-06-12 16:43:26 -0400

    devnet-2641-step-2-new:
    Change-Id: If3a5e1f01d43ca7228bb3a2b17bb05458717bbdb

--- example_apps/python/pkg/download/asa_configs.py
+++ example_apps/python/pkg/download/asa_configs.py
@@ -4,4 +4,5 @@ from . import cdoapi
 
 API_TOKEN=os.getenv('API_TOKEN')
 def download_asa_configs(api_token, output_dir):
-    cdoapi.get_devices(API_TOKEN)
+    devices = cdoapi.get_devices(API_TOKEN)
+    cdoapi.save_device_configs(devices, output_dir)
--- example_apps/python/pkg/download/cdoapi.py
+++ example_apps/python/pkg/download/cdoapi.py
@@ -17,6 +17,7 @@ PROD_URL = 'https://www.defenseorchestrator.com'
 
 def get_devices(api_token):
     download_msg = as_in_progress_msg('Downloading configurations for ASAs from CDO...') 
+    print(as_in_progress_msg(download_msg), end='\r')
     params = {
         'q': '(deviceType:ASA)',
     }
@@ -31,12 +32,18 @@ def get_devices(api_token):
 
     devices = json.loads(response.text)    
     print(as_done_msg(download_msg)) 
+    return devices
 
 
 def _build_url(namespace, type):
     return PROD_URL + '/aegis/rest/v1/services/' + namespace + '/' + type
 
+def save_device_configs(devices, output_dir):
+    for device in devices:
+        _save_device_config(device['name'], device['deviceConfig'], output_dir)
 
-def save_device_config(device_name, device_config, output_dir):
+def _save_device_config(device_name, device_config, output_dir):
+    print(as_in_progress_msg('Saving ' + device_name +' to disk...'), end='')
     output_file = open(os.path.join(output_dir, device_name), 'w')
     output_file.write(device_config)
+    print(as_done_msg(''))
