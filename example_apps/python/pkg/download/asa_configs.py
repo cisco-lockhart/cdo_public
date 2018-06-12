60dd18cb94d29bf52d452908f1d08d243d13d6a0
Siddhu Warrier <siwarrie@cisco.com>
2018-06-12 16:28:28 -0400

    devnet-2641-step-1: - Make it simpler
    Change-Id: I6f8728c8386d3a4f4cd73455324a916cd5697155

--- example_apps/python/pkg/download/asa_configs.py
+++ example_apps/python/pkg/download/asa_configs.py
@@ -1,35 +1,7 @@
-#!/usr/bin/env python
 
-import requests
-import json
 import os
-import sys
-from ..utils import *
+from . import cdoapi
 
-from .. import envutils
-from sty import fg, ef, rs
-
-
-NUM_DEVICES_TO_RETRIEVE_PER_QUERY = 50
-
-PROD_URL = 'https://www.defenseorchestrator.com'
-
-
+API_TOKEN=os.getenv('API_TOKEN')
 def download_asa_configs(api_token, output_dir):
-    download_msg = as_in_progress_msg('Downloading configurations for ASAs from CDO...')
-    print(download_msg, end='')
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
-
-    # YOUR CODE GOES HERE
-
-    print(as_done_msg(''))
-
-
-def _build_url(namespace, type):
-    return PROD_URL + '/aegis/rest/v1/services/' + namespace + '/' + type
-
-
-def _save_device_config(device_name, device_config, output_dir):
-    output_file = open(os.path.join(output_dir, device_name), 'w')
-    output_file.write(device_config)
+    print("Write your code here")
---
+++ example_apps/python/pkg/download/cdoapi.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import requests
+import json
+import os
+import sys
+from ..utils import *
+
+from .. import envutils
+from sty import fg, ef, rs
+
+
+NUM_DEVICES_TO_RETRIEVE_PER_QUERY = 50
+
+PROD_URL = 'https://www.defenseorchestrator.com'
+
+
+def get_devices(api_token):
+    download_msg = as_in_progress_msg('Downloading configurations for ASAs from CDO...') 
+    params = {
+        'q': '(deviceType:ASA)',
+    }
+    headers = {
+        "Authorization": "Bearer " + api_token,
+        "Content-Type": "application/json"
+    }
+
+    response = requests.get(url=_build_url(namespace='targets', type='devices'),
+                            headers=headers,
+                            params=params)
+
+    devices = json.loads(response.text)    
+    print(as_done_msg(download_msg)) 
+
+
+def _build_url(namespace, type):
+    return PROD_URL + '/aegis/rest/v1/services/' + namespace + '/' + type
+
+
+def save_device_config(device_name, device_config, output_dir):
+    output_file = open(os.path.join(output_dir, device_name), 'w')
+    output_file.write(device_config)
