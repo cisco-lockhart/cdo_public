edd456cb79fe4bfdd6df4eba8e5daf046fca93a1
Siddhu Warrier <siwarrie@cisco.com>
2018-06-13 09:31:23 -0400

    devnet-2641-step-1: - Create directory if not exists
    Change-Id: Ic24b828495d9ce10319a19bc9b48163ef222c166

--- example_apps/python/pkg/download/asa_configs.py
+++ example_apps/python/pkg/download/asa_configs.py
@@ -3,5 +3,8 @@ import os
 from . import cdoapi
 
 API_TOKEN=os.getenv('API_TOKEN')
+
 def download_asa_configs(api_token, output_dir):
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
     print("Write your code here")
